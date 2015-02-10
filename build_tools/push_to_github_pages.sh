c78a0852d781c17b66f71d32332d9597c2a11f30
Chris Hill-Scott <me@quis.cc>
2015-02-10 09:56:12 +0000

    Push ./pages to gh-pages programatically
    This commit adds a script which:
    - Deletes everything except the ./pages folder
    - Moves the contents of the pages folder to ./
    - Cleans, commits to and pushes the gh-pages branch
    - Returns the user to the branch which they were originally working on

--- build_tools/push_to_github_pages.sh
+++ build_tools/push_to_github_pages.sh
@@ -1,1 +1,35 @@
-find . | grep -v "pages" | rm
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+destination_branch=gh-pages
+echo "================================================================================"
+echo "Pushing documentation to Github Pages from" $(git rev-parse --abbrev-ref HEAD)
+echo "--------------------------------------------------------------------------------"
+if [ $(ls ./pages | grep ".html" | wc -l) -eq 0 ]; then
+  echo "ERROR: You need to generate the static pages first\nUse python build_tools/publish-gh-page.py\n"; exit
+else
+  echo "Static pages found in ./pages"
+fi
+if [ $(git status --porcelain | wc -l) -gt 0 ]; then
+  echo "ERROR: You have uncommitted changes which would be lost by running this script"; exit
+fi
+echo
+echo "--------------------------------------------------------------------------------"
+echo "Deleting stuff that isn't needed for Github Pages"
+echo "--------------------------------------------------------------------------------"
+find . | grep -v "pages" | grep -v ".git" | xargs rm -rf
+mv pages/* ./
+rm -r pages
+echo "--------------------------------------------------------------------------------"
+echo "Deleting gh-pages branch and creating a clean version"
+echo "--------------------------------------------------------------------------------"
+git branch -D $destination_branch
+git checkout -b $destination_branch
+git add .
+git commit -am "Generate toolkit documentation"
+echo "--------------------------------------------------------------------------------"
+echo "Force pushing to origin/$destination_branch"
+echo "--------------------------------------------------------------------------------"
+git push -f origin $destination_branch
+echo "--------------------------------------------------------------------------------"
+echo "Returning you to $current_branch"
+echo "--------------------------------------------------------------------------------"
+git checkout -f $current_branch
