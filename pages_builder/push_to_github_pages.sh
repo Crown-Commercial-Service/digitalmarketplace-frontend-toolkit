9bbe884f1a0554232b9705fb774281bbda5b8662
Chris Hill-Scott <me@quis.cc>
2015-02-16 11:39:35 +0000

    Rewrite publishing script to preserve history
    This commit changes the behaviour of the script to NOT nuke the gh-pages
    branch when generating the documentation.
    
    It also makes it explicit where the generated pages have come from (in the
    header of `./pages/index.html`).

--- pages_builder/push_to_github_pages.sh
+++ pages_builder/push_to_github_pages.sh
@@ -1,36 +1,36 @@
+#!/bin/sh
 set -e
 current_branch=$(git rev-parse --abbrev-ref HEAD)
+build_from=$1
+destination_repo=git@github.com:alphagov/digitalmarketplace-frontend-toolkit.git
 destination_branch=gh-pages
 echo "================================================================================"
-echo "Pushing documentation to Github Pages from" $(git rev-parse --abbrev-ref HEAD)
+echo "Checking out" $build_from
 echo "--------------------------------------------------------------------------------"
-if [ $(ls ./pages | grep ".html" | wc -l) -eq 0 ]; then
-  echo "ERROR: You need to generate the static pages first\nUse python build_tools/publish-gh-page.py\n"; exit
-else
-  echo "Static pages found in ./pages"
-fi
 if [ $(git status --porcelain | wc -l) -gt 0 ]; then
   echo "ERROR: You have uncommitted changes which would be lost by running this script"; exit
 fi
-echo
+git checkout $build_from
+git reset --hard HEAD
+echo "================================================================================"
+echo "Putting branch $destination_branch from $destination_repo into ./pages"
 echo "--------------------------------------------------------------------------------"
-echo "Deleting stuff that isn't needed for Github Pages"
+rm -rf ./pages
+mkdir pages
+cd ./pages
+git clone $destination_repo ./
+git checkout $destination_branch
 echo "--------------------------------------------------------------------------------"
-ls -A ./ | grep -v "pages" | grep -v ".git" | xargs rm -rf
-mv pages/* ./
-rm -r pages
+echo "Generating pages"
 echo "--------------------------------------------------------------------------------"
-echo "Deleting gh-pages branch and creating a clean version"
+rm -rf ./*
+cd ..
+python ./pages_builder/generate_pages.py
+sed -i -e "s/\[VERSION\]/Digital Marketplace Frontend Toolkit @ $build_from/g" ./pages/index.html
 echo "--------------------------------------------------------------------------------"
-git branch -D $destination_branch
-git checkout -b $destination_branch
+echo "Commiting changes to generated pages"
+echo "--------------------------------------------------------------------------------"
+cd pages
 git add .
-git commit -am "Generate toolkit documentation"
-echo "--------------------------------------------------------------------------------"
-echo "Force pushing to origin/$destination_branch"
-echo "--------------------------------------------------------------------------------"
-git push -f origin $destination_branch
-echo "--------------------------------------------------------------------------------"
-echo "Returning you to $current_branch"
-echo "--------------------------------------------------------------------------------"
-git checkout -f $current_branch
+git commit -m "Publishing Digital Marketplace Frontend Toolkit documentation from $build_from"
+git push origin $destination_branch
