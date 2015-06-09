e68e66ed3ad7a026241ec62c1f91aff8e5a644db
Chris Hill-Scott <me@quis.cc>
2015-06-09 16:55:13 +0100

    Avoid bashisms, duplicate 'v's

--- pages_builder/tag_latest_version.sh
+++ pages_builder/tag_latest_version.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
 set -e
 echo "≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡"
-if [[ $(git log master --pretty=oneline --abbrev-commit --no-decorate | grep "Bump version to" | head -n1 | wc -l) -gt 0 ]]; then
-  echo "TAGGING LATEST VERSION"
-  echo "--------------------------------------------------------------------------------"
-  commit=$(git log master --pretty=oneline --abbrev-commit --no-decorate | grep "Bump version to" | head -n1)
-  commit_message_parts=($commit)
-  sha=${commit_message_parts[0]}
-  version=v$(cat VERSION.txt)
-  previous_version=$(git tag|tail -n1)
-  changes=$(git log --merges --oneline $previous_version..$version)
-  echo Commit:\ \ \ $commit
-  echo Version:\ \ $version
-  echo Previous:\ $previous_version
-  echo "================================================================================"
-  echo "CHANGES:"
-  echo $changes
-  echo "================================================================================"
-  git tag -a v$version -m "Release version $version
+echo "TAGGING LATEST VERSION"
+echo "--------------------------------------------------------------------------------"
+commit=$(git log master --pretty=oneline --abbrev-commit --no-decorate | grep "Bump version to" | head -n1)
+sha=$(echo $commit | cut -d ' ' -f1)
+version=v$(cat VERSION.txt)
+previous_version=$(git tag|tail -n1)
+changes=$(git log --merges --oneline $previous_version..$sha)
+echo Commit:\ \ \ $commit
+echo Version:\ \ $version
+echo Previous:\ $previous_version
+echo "================================================================================"
+echo "CHANGES:"
+echo $changes
+echo "================================================================================"
+git tag -a $version -m "Release version $version
 
 Includes:
 $changes"
-  echo "New tag created!"
-  echo "--------------------------------------------------------------------------------"
-else
-  echo "No version bump found"
-fi
+echo "New tag created!"
+echo "--------------------------------------------------------------------------------"
