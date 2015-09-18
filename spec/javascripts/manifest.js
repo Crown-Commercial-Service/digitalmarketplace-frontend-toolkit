b6cc302ab24b63acc7c8a82d9422300703dcaeeb
Tom Byers <tombaromba@gmail.com>
2015-09-18 17:04:46 +0100

    Add a Jasmine testrunner for the JS tests

---
+++ spec/javascripts/manifest.js
@@ -0,0 +1,18 @@
+// files are loaded from the /spec/javascripts/support folder so paths are relative to that
+var manifest = {
+  support : [
+    'vendor/jquery.js',
+    'vendor/hogan.js',
+    '../../../node_modules/govuk_frontend_toolkit/javascripts/vendor/polyfills/bind.js',
+    '../../../toolkit/javascripts/list-entry.js',
+    '../../../toolkit/javascripts/word-counter.js'
+  ],
+  test : [
+    '../unit/wordCountSpec.js',
+    '../unit/listEntrySpec.js'
+  ]
+};
+
+if (typeof exports !== 'undefined') {
+  exports.manifest = manifest;
+}
