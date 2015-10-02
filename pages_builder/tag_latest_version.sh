a0d61b07b873c24b83533617ac2ea6620e27d518
Chris Hill-Scott <me@quis.cc>
2015-10-02 14:04:12 +0100

    Appease Jenkins

--- pages_builder/tag_latest_version.sh
+++ pages_builder/tag_latest_version.sh
@@ -9,7 +9,7 @@ commit=$(git log master --pretty=oneline --abbrev-commit --no-decorate | grep "B
 sha=$(echo $commit | cut -d ' ' -f1)
 version=v$(cat VERSION.txt)
 previous_version=$(git describe --abbrev=0)
-changes=$(git log --merges --oneline $previous_version..head)
+changes=$(git log --merges --oneline $previous_version...master)
 echo Commit:\ \ \ $commit
 echo Version:\ \ $version
 echo Previous:\ $previous_version
