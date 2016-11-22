aef78dcd3d444d4fd17212a936922a0a7a2c4672
Tom Byers <tombaromba@gmail.com>
2016-11-22 16:10:14 +0000

    Allow module loader to be called multiple times

--- toolkit/javascripts/module-loader.js
+++ toolkit/javascripts/module-loader.js
@@ -2,50 +2,107 @@
 
   "use strict";
 
-  var module;
-
-  if(typeof console === 'undefined') {
-    console = {
-      log: function () {},
-      time: function () {},
-      timeEnd: function () {}
-    };
-  }
-
-  if (
-    (GDM.debug = !window.location.href.match(/gov.uk/) && !window.jasmine)
-  ) {
-    console.log(
-      "%cDebug mode %cON",
-      "color:#550; background:yellow; font-size: 11pt",
-      "color:yellow; background: #550;font-size:11pt"
-    );
-    console.time("Modules loaded");
-  }
-
-  // Initialise our modules
-  for (module in GDM) {
-
-    if (GDM.debug && module !== "debug") {
-      console.log(
-        "%cLoading module %c" + module,
-        "color:#6a6; background:#dfd; font-size: 11pt",
-        "color:#dfd; background:green; font-size: 11pt"
-      );
+  var ModuleLoader = function () {
+    if (typeof GDM.debug !== 'undefined') {
+      GDM.debug = !window.location.href.match(/gov.uk/) && !window.jasmine;
     }
+    this.cache = [];
+    this.console.init();
+  };
 
-    if ("function" === typeof GDM[module].init) {
+  // Wrapper for calls to global console method
+  ModuleLoader.prototype.console = {
+    init: function () {
+
+      // Older browers don't have window.console to stub it if so
+      if(typeof console === 'undefined') {
+        console = {
+          log: function () {},
+          time: function () {},
+          timeEnd: function () {}
+        };
+      }
+
+      if (GDM.debug) {
+        console.log(
+          "%cDebug mode %cON",
+          "color:#550; background:yellow; font-size: 11pt",
+          "color:yellow; background: #550;font-size:11pt"
+        );
+        console.time("Modules loaded");
+      }
+
+    },
+    log: function (module) {
+
+      if (GDM.debug && module !== "debug") {
+        console.log(
+          "%cLoading module %c" + module,
+          "color:#6a6; background:#dfd; font-size: 11pt",
+          "color:#dfd; background:green; font-size: 11pt"
+        );
+      }
+
+    }
+  };
+
+  // Load the a module from the namespace, if it fits our criteria
+  ModuleLoader.prototype.loadModule = function (module, namespace) {
+
+    if ("function" === typeof namespace[module].init) {
       // If a module has an init() method then we want that to be called here
-      GDM[module].init();
-    } else if ("function" === typeof GDM[module]) {
+      namespace[module].init();
+    } else if ("function" === typeof namespace[module]) {
       // If a module doesn't have an interface then call it directly
-      GDM[module]();
+      namespace[module]();
     }
 
-  }
+  };
+
+  // Check if a module has loaded already
+  ModuleLoader.prototype.moduleIsLoaded = function (module, namespace) {
+
+    var namespaceCached = (typeof this.cache[namespace] !== 'undefined'),
+        moduleLoaded = $.inArray(module, this.cache[namespace]) > -1;
+
+    return namespaceCached && moduleLoaded;
+
+  };
+
+  // cache the module against its namespace
+  ModuleLoader.prototype.addToCache = function (module, namespace) {
+
+    if (typeof this.cache[namespace] === 'undefined') {
+      this.cache[namespace] = [];
+    }
+    this.cache[namespace].push(module);
+
+  };
+  ModuleLoader.prototype.loadModulesFromNamespace = function (namespace) {
+
+    var module;
+
+    // Initialise all modules in namespace
+    for (module in namespace) {
+
+      // Load any modules not already loaded
+      if (!this.moduleIsLoaded(module, namespace)) {
+        this.console.log(module);
+        this.loadModule(module)
+        this.addToCache(module, namespace);
+      }
+
+    }
+    if (GDM.debug) console.timeEnd("Modules loaded");
+
+  };
 
   GOVUK.GDM = GDM;
 
-  if (GDM.debug) console.timeEnd("Modules loaded");
+  // Store an instance of ModuleLoader on GDM
+  GOVUK.GDM.moduleLoader = new ModuleLoader();
+
+  // Load all modules already added to GOVUK.GDM
+  GOVUK.GDM.moduleLoader.loadModulesFromNamespace(GDM);
 
 }).apply(this, [GOVUK||{}, GOVUK.GDM||{}]);
