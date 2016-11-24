24656fda9cca97f9e3a09ff061a013e6cadecf42
Tom Byers <tombaromba@gmail.com>
2016-11-24 11:40:50 +0000

    Make loader only work on parent namespace
    It's initialised by an instance of it being
    attached to the namespace anyway.

--- toolkit/javascripts/module-loader.js
+++ toolkit/javascripts/module-loader.js
@@ -1,11 +1,12 @@
-(function(GOVUK, GDM) {
+(function(root, namespace) {
 
   "use strict";
 
-  var ModuleLoader = function () {
-    if (typeof GDM.debug !== 'undefined') {
-      GDM.debug = !window.location.href.match(/gov.uk/) && !window.jasmine;
+  var ModuleLoader = function (namespace) {
+    if (typeof namespace.debug !== 'undefined') {
+      namespace.debug = !window.location.href.match(/gov.uk/) && !window.jasmine;
     }
+    this.namespace = namespace;
     this.cache = [];
     this.console.init();
   };
@@ -15,28 +16,28 @@
     init: function () {
 
       // Older browers don't have window.console to stub it if so
-      if(typeof console === 'undefined') {
-        console = {
+      if(typeof root.console === 'undefined') {
+        root.console = {
           log: function () {},
           time: function () {},
           timeEnd: function () {}
         };
       }
 
-      if (GDM.debug) {
-        console.log(
+      if (namespace.debug) {
+        root.console.log(
           "%cDebug mode %cON",
           "color:#550; background:yellow; font-size: 11pt",
           "color:yellow; background: #550;font-size:11pt"
         );
-        console.time("Modules loaded");
+        root.console.time("Modules loaded");
       }
 
     },
     log: function (module) {
 
-      if (GDM.debug && module !== "debug") {
-        console.log(
+      if (namespace.debug && module !== "debug") {
+        root.console.log(
           "%cLoading module %c" + module,
           "color:#6a6; background:#dfd; font-size: 11pt",
           "color:#dfd; background:green; font-size: 11pt"
@@ -46,63 +47,49 @@
     }
   };
 
-  // Load the a module from the namespace, if it fits our criteria
-  ModuleLoader.prototype.loadModule = function (module, namespace) {
+  // Load the a module, if it fits our criteria
+  ModuleLoader.prototype.loadModule = function (module) {
 
-    if ("function" === typeof namespace[module].init) {
+    if ("function" === typeof this.namespace[module].init) {
       // If a module has an init() method then we want that to be called here
-      namespace[module].init();
-    } else if ("function" === typeof namespace[module]) {
+      this.namespace[module].init();
+    } else if ("function" === typeof this.namespace[module]) {
       // If a module doesn't have an interface then call it directly
-      namespace[module]();
+      this.namespace[module]();
     }
 
   };
 
   // Check if a module has loaded already
-  ModuleLoader.prototype.moduleIsLoaded = function (module, namespace) {
+  ModuleLoader.prototype.moduleIsLoaded = function (module) {
 
-    var namespaceCached = (typeof this.cache[namespace] !== 'undefined'),
-        moduleLoaded = $.inArray(module, this.cache[namespace]) > -1;
-
-    return namespaceCached && moduleLoaded;
+    return $.inArray(module, this.cache) > -1;
 
   };
 
-  // cache the module against its namespace
-  ModuleLoader.prototype.addToCache = function (module, namespace) {
-
-    if (typeof this.cache[namespace] === 'undefined') {
-      this.cache[namespace] = [];
-    }
-    this.cache[namespace].push(module);
-
-  };
-  ModuleLoader.prototype.loadModulesFromNamespace = function (namespace) {
+  ModuleLoader.prototype.loadModulesInNamespace = function () {
 
     var module;
 
     // Initialise all modules in namespace
-    for (module in namespace) {
+    for (module in this.namespace) {
 
       // Load any modules not already loaded
-      if (!this.moduleIsLoaded(module, namespace)) {
+      if (!this.moduleIsLoaded(module)) {
         this.console.log(module);
         this.loadModule(module)
-        this.addToCache(module, namespace);
+        this.cache.push(module);
       }
 
     }
-    if (GDM.debug) console.timeEnd("Modules loaded");
+    if (namespace.debug) console.timeEnd("Modules loaded");
 
   };
 
-  GOVUK.GDM = GDM;
-
   // Store an instance of ModuleLoader on GDM
-  GOVUK.GDM.moduleLoader = new ModuleLoader();
+  namespace.moduleLoader = new ModuleLoader(namespace);
 
   // Load all modules already added to GOVUK.GDM
-  GOVUK.GDM.moduleLoader.loadModulesFromNamespace(GDM);
+  namespace.moduleLoader.loadModulesInNamespace();
 
-}).apply(this, [GOVUK||{}, GOVUK.GDM||{}]);
+}).apply(this, [window, GOVUK.GDM]);
