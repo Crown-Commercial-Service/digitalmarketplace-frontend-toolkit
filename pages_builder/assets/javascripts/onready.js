dca6221a1276d95f78f3b6f3d2abbaba5921d62d
Paul Craig <paul_craig_16@hotmail.com>
2017-02-15 00:12:34 +0000

    Add 'button' role to link button template
    Another of the changes pulled in in the latest
    template is that links with a role="button" can be targeted
    by a script that will treat them the same as buttons.
    This means you can press the space bar when they're focused
    and it should register a click.
    
    Frontend apps will have to set up their javascript independently,
    but the button role will be in the markup from now on.
    
    Source: https://github.com/alphagov/govuk_frontend_toolkit/pull/297

--- pages_builder/assets/javascripts/onready.js
+++ pages_builder/assets/javascripts/onready.js
@@ -32,9 +32,15 @@
     GOVUK.GDM.validation();
   }
 
+  if (GOVUK.shimLinksWithButtonRole) {
+    GOVUK.shimLinksWithButtonRole.init({
+      selector: '[class^=link-button]'
+    });
+  }
+
   if (GOVUK.ShowHideContent) {
-    var showHideContent = new GOVUK.ShowHideContent()
-    showHideContent.init()
+    var showHideContent = new GOVUK.ShowHideContent();
+    showHideContent.init();
   }
 
 })(window);
