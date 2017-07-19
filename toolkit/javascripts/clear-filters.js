6c5196203e0718d4609c15ab2444e9b98bdd0e34
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-07-19 17:54:25 +0100

    Adds in clear-filters.js file which handles clearing of filters.
    Ticket: https://trello.com/c/IrcO0G3U/491-add-clear-all-filters-link

---
+++ toolkit/javascripts/clear-filters.js
@@ -0,0 +1,42 @@
+/* 
+  ClearFilters
+
+    Requirements
+      ID: #dm-clear-all-filters 
+      class: .dm-filters
+*/
+
+(function() {
+  "use strict";
+
+  var ClearFilters = function( clearFilterLink ) {
+    this.$clearFilterLink = clearFilterLink;
+    this.initaliseListener();
+  };
+
+  ClearFilters.init = function() {};
+
+  ClearFilters.prototype.initaliseListener = function() {
+    
+    var _this = this;
+
+    this.$clearFilterLink.on('click', function(event) {
+        event.preventDefault();
+        _this.clearFilters();
+    });
+    
+  };
+
+  ClearFilters.prototype.clearFilters = function() {
+    $('.dm-filters input[type=checkbox]:checked')
+      .prop('checked', false)
+      .trigger('change');
+  };
+
+  GOVUK = GOVUK || {};
+  GOVUK.GDM = GOVUK.GDM || {};
+  GOVUK.GDM.ClearFilters = ClearFilters;
+
+  var clearFilters = new GOVUK.GDM.ClearFilters( $('#dm-clear-all-filters') );
+  
+})();
