cb260d6324990b513323f3a27e1caacefca49c4a
George Lund <george.lund@digital.cabinet-office.gov.uk>
2017-09-27 11:42:07 +0100

    Provide single point of integration for site-wide JS and SCSS.
     - currently this only covers the feedback forms JS/SCSS, but in
       principle any CSS or JS that is required by the base template
       should now be included here.
     - this means that consumers of the toolkit need only import one
       JS/SCSS file and they know that their base template will continue
       to work correctly;
     - the toolkit can then continue to be upgraded without risk of the
       base template breaking due to missing imports/includes.
    
    https://trello.com/c/Uak7y047/706-feedback-forms

---
+++ toolkit/javascripts/site.js
@@ -0,0 +1,5 @@
+// All Digital Marketplace apps should include this file in their `application.js`.
+// Any Javascript modules that are required in order to use the `base-page.html` template should be listed here.
+// TODO there should be more imports here, and fewer in the separate application.js files.
+
+//= require report-a-problem.js
