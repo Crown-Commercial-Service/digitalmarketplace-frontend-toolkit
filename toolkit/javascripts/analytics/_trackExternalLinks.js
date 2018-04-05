ce38a443d76a7daa62978d1f728b8444b30f8a70
Dilwoar Hussain <DilwoarH@users.noreply.github.com>
2018-04-05 15:27:57 +0100

    updates category

--- toolkit/javascripts/analytics/_trackExternalLinks.js
+++ toolkit/javascripts/analytics/_trackExternalLinks.js
@@ -9,7 +9,7 @@
         return this.hostname && this.hostname !== GOVUK.GDM.analytics.location.hostname();
       }).on('click', function (e) {
         var $target = $(e.target);
-        var category = 'external-link';
+        var category = 'external-link-all';
         var action = $target.text();
         var label = $target.attr('href');
         GOVUK.GDM.analytics.events.sendEvent(category, action, label);
