05ae0be6b5a1a6dec1058163e7ccef59a2990fdc
Dilwoar Hussain <DilwoarH@users.noreply.github.com>
2018-04-05 10:45:12 +0100

    Update _trackExternalLinks.js

--- toolkit/javascripts/analytics/_trackExternalLinks.js
+++ toolkit/javascripts/analytics/_trackExternalLinks.js
@@ -6,7 +6,7 @@
 
     trackExternalLinks: function () {
       $('a').filter(function () {
-        return this.hostname && this.hostname !== location.hostname;
+        return this.hostname && this.hostname !== GOVUK.GDM.analytics.location.hostname();
       }).on('click', function (e) {
         var $target = $(e.target);
         var category = 'external-link';
